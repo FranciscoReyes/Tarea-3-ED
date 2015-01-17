5a6c7875f916142680a22e04229e91d16e0cd94d
Usuario <Usuario@Acer-PC>
2015-01-17 16:24:52 +0100

    Tarea 3 ED, pruebas de código

---
+++ test/practicaunidad3/CCuentaTest.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaunidad3;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Francisco A. Reyes
+ */
+public class CCuentaTest {
+    
+    public CCuentaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of asignarNombre method, of class CCuenta.
+     */
+    @Test
+    public void testAsignarNombre() {
+        System.out.println("asignarNombre");
+        String nom = "";
+        CCuenta instance = new CCuenta();
+        instance.asignarNombre(nom);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of obtenerNombre method, of class CCuenta.
+     */
+    @Test
+    public void testObtenerNombre() {
+        System.out.println("obtenerNombre");
+        CCuenta instance = new CCuenta();
+        String expResult = "";
+        String result = instance.obtenerNombre();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of estado method, of class CCuenta.
+     */
+    @Test
+    public void testEstado() {
+        System.out.println("estado");
+        CCuenta instance = new CCuenta();
+        double expResult = 0.0;
+        double result = instance.estado();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of ingresar method, of class CCuenta.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testIngresar() throws Exception {
+        System.out.println("ingresar");
+        double cantidad = 10.0;
+        CCuenta instance = new CCuenta();
+        instance.ingresar(cantidad);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of retirar method, of class CCuenta.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testRetirar() throws Exception {
+        System.out.println("retirar");
+        double cantidad = 0.0;
+        CCuenta instance = new CCuenta();
+        instance.retirar(cantidad);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of obtenerCuenta method, of class CCuenta.
+     */
+    @Test
+    public void testObtenerCuenta() {
+        System.out.println("obtenerCuenta");
+        CCuenta instance = new CCuenta();
+        String expResult = "";
+        String result = instance.obtenerCuenta();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    private void assertEquals(String expResult, String result) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void assertEquals(double expResult, double result, double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
